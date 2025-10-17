a14dcaceab68d7cd9974f88c548bd4b4c5a74977
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-10-17 17:44:27 +0200

    Fix: escape in grep

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -euo pipefail
 
 output=$(find ./mains -maxdepth 1 -type f -name 'main*.cpp' -print0 | sort -z -udf | xargs -0 -n1 sh -c 'g++ "$1" -iquote . --std=c++20 && ./a.out' sh)
 echo "$output"
 
-if echo "$output" | grep -q '^[FAIL]'; then
+if echo "$output" | grep -q '^\[FAIL\]'; then
     exit 84
 fi
