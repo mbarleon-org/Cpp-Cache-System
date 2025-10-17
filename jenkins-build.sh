a3e23be7d3f50d569711ea3bf7694a56fea1ab1e
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-10-17 17:37:39 +0200

    Fix: added set -euo pipefail

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 output=$(find ./mains -maxdepth 1 -type f -name 'main*.cpp' -print0 | sort -z -udf | xargs -0 -n1 sh -c 'g++ "$1" -iquote . --std=c++20 && ./a.out' sh)
 echo "$output"
